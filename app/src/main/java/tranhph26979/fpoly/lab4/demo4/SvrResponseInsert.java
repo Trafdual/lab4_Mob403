091e80bee4a177168903c793b71e3a0d69cb2ae3
trafdual <you@example.com>
2023-07-21 18:08:45 +0700

    first commit

---
+++ app/src/main/java/tranhph26979/fpoly/lab4/demo4/SvrResponseInsert.java
@@ -0,0 +1,14 @@
+package tranhph26979.fpoly.lab4.demo4;
+
+public class SvrResponseInsert {//GET
+    private Prd products;//khong duoc viet sai ten bang
+    private String message;
+
+    public Prd getProducts() {
+        return products;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
